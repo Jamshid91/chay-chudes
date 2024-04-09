eebd03ad98d171524bb5283fc3e5ac2eabd87d31
Jamshid91 <iymon0217@gmail.com>
2024-04-09 16:46:41 +0500

    added

--- assets/js/sale.js
+++ assets/js/sale.js
@@ -10,12 +10,27 @@ const sale_products = new Swiper('.swiper-sale-products', {
 });
 
 const sale = new Swiper('.swiper-sale', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 6,
+  loop: false,
   navigation: {
     nextEl: '.sale-next',
     prevEl: '.sale-prev',
   },
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+        grid: {
+            rows: 1,
+            fill: 'row'
+          },
+    },
+    767: {
+      slidesPerView: 3, 
+      spaceBetween: 45,
+      grid: {
+          rows: 3,
+          fill: 'row'
+        },
+    },
+}
 
 });
